0baa1a9a0f6c2c39aa6d1a04215c6b13b376a5b6
krupikivan <krupikivan@gmail.com>
2019-07-14 12:08:31 -0300

    Pagina comentarios y cambio de ruteo

--- lib/widget/reviews_widget.dart
+++ lib/widget/reviews_widget.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movies/bloc/reviews/movie_review_bloc.dart';
 import 'package:flutter_movies/bloc/reviews/movie_review_provider.dart';
+import 'package:flutter_movies/functions/global_state.dart';
 import 'package:flutter_movies/model/movie_reviews.dart';
 
 class ReviewWidget extends StatelessWidget {
 
-  final int id;
-
-  const ReviewWidget({this.id});
+  GlobalState _store = GlobalState.instance;
 
   @override
   Widget build(BuildContext context) {
     final MovieReviewBloc _reviewsMovieBloc = MovieReviewProvider.of(context);
-    _reviewsMovieBloc.getMovieReviews(id);
+    _reviewsMovieBloc.getMovieReviews(_store.get('idMovie'));
     return Scaffold(
       appBar: AppBar(
         title: Text('Comentarios'),
       ),
       body: Container(
-        color: Colors.white,
         child: StreamBuilder<MovieReviews>(
             stream: _reviewsMovieBloc.outputReviews,
             builder: (BuildContext context, AsyncSnapshot<MovieReviews> snapshot) {
-              return ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: snapshot.data.results.length,
-                  itemBuilder: (BuildContext context, int index){
-                   return ListTile(
-                     title: Text(snapshot.data.results[index].author),
-                     subtitle: Text(snapshot.data.results[index].content),
-                   );
-                  }
+              return ListView.separated(
+                scrollDirection: Axis.vertical,
+                itemCount: snapshot.data.results.length,
+                itemBuilder: (BuildContext context, int index){
+                  return ListTile(
+                    title: Text(snapshot.data.results[index].author, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), textScaleFactor: 1.4),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Text(snapshot.data.results[index].content, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                    ),
+                    dense: false,
+                    contentPadding: EdgeInsets.all(15),
+                  );
+                }, separatorBuilder: (BuildContext context, int index) {return Divider();},
               );
             }
-          ),
+        ),
       ),
     );
   }
